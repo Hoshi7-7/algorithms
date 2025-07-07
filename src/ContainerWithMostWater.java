96c15dd89530969176308ce38f3a301b5ec64195
Hoshi7-7 <74125441+Hoshi7-7@users.noreply.github.com>
2025-07-07 14:06:41 -0600

    add container with most water solution

---
+++ src/ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+public class ContainerWithMostWater {
+
+    /*
+    leetcode link: https://leetcode.com/problems/container-with-most-water/
+     */
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        // Using two pointers to find the maximum area
+        // Time complexity: O(n), Space complexity: O(1)
+        while (left < right) {
+            int width = right - left;
+            int minHeight = Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, width * minHeight);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
