abc1ed4e411492cdbdd7d8705280a009b99586b9
Hoshi7-7 <74125441+Hoshi7-7@users.noreply.github.com>
2025-07-15 14:44:03 -0600

    adding letcode url problem and big O complexity comments

--- src/TrappingRainWater.java
+++ src/TrappingRainWater.java
@@ -1,5 +1,13 @@
 class TrappingRainWater {
+
+    /*
+     leetcode link: https://leetcode.com/problems/trapping-rain-water/?envType=company&envId=amazon&favoriteSlug=amazon-thirty-days
+     */
     public int trap(int[] height) {
+        /*
+        Time complexity: O(n), Space complexity: O(1)
+        This solution uses a two-pointer approach to calculate the amount of water that can be trapped
+         */
         int left = 0, right = height.length - 1;
         int max_left = 0,max_right = 0;
         int ans = 0;
