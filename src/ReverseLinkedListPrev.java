c8f02e4f5e1f96d17ce3dc6e2f4d2acd340222e8
Hoshi7-7 <74125441+Hoshi7-7@users.noreply.github.com>
2025-07-20 13:18:21 -0600

    add reverse linked list solutions

---
+++ src/ReverseLinkedListPrev.java
@@ -0,0 +1,25 @@
+public class ReverseLinkedListPrev {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    /*
+        https://leetcode.com/problems/reverse-linked-list/description/
+        Time complexity: O(n), where n is the number of nodes in the linked list.
+        Space complexity: O(1), since we are using only a few pointers.
+        This solution reverses the linked list in place by iterating through the list and reversing the pointers.
+    */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr!=null){
+            ListNode aux = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = aux;
+        }
+
+        return prev;
+    }
+}
---
+++ src/ReverseLinkedListStack.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class ReverseLinkedListStack {
+// Definition for singly-linked list.
+public static class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+/*
+    https://leetcode.com/problems/reverse-linked-list/description/
+    Time complexity: O(n), where n is the number of nodes in the linked list.
+    Space complexity: O(n), due to the stack used to store the values of the nodes.
+    This solution uses a stack to reverse the linked list by pushing all node values onto the stack
+    and then popping them back into the nodes in reverse order.
+
+ */
+    public ListNode reverseList(ListNode head) {
+        int cont = 0;
+        ListNode aux = head;
+        ListNode tmp = head;
+        Stack<Integer> values = new Stack<>();
+        while(aux!=null){
+            values.push(aux.val);
+            aux = aux.next;
+        }
+
+        while(tmp!=null){
+            tmp.val = values.pop();
+            tmp=tmp.next;
+        }
+
+        return head;
+    }
+}
